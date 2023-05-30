ad45d5dca60ee736485b575f1d69d1cea90270df
Muhammad Huzaifa Khan <57367781+HuzaifaKhan20@users.noreply.github.com>
2023-05-30 15:43:12 +0500

    Add files via upload

--- xk-forum/index.php
+++ xk-forum/index.php
@@ -1,26 +1,99 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Welcome to XK forum - tech forum</title>
-  </head>
-  <body>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <title>Welcome to XK forum - tech forum</title>
+  <style>
+    .carousel-item {
+      height: 60vh !important;
+    }
+
+    .card img {
+      height: 191px;
+    }
+  </style>
+</head>
+
+<body>
   <?php include 'partials/_header.php'; ?>
+  <?php include 'partials/_dbconnect.php'; ?>
 
-  <div class="container">
-    <h2>Welcome to iDiscuss - Categories</h2>
+  <!-- slider starts here -->
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://source.unsplash.com/random/400X500/?apple,code" class="d-block w-100 img-fluid" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://source.unsplash.com/random/400X500/?programmer,microsoft" class="d-block w-100 img-fluid" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://source.unsplash.com/random/400X500/?code,java" class="d-block w-100 img-fluid" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
-  
+
+
+  <!-- Category container starts here -->
+  <div class="container my-3">
+    <h2 class="text-center">Welcome to iDiscuss - Categories</h2>
+    <div class="row my-4 gy-5">
+
+    <!-- fetch all the categories  -->
+    <?php
+
+      $sql = "SELECT * FROM `categories`";
+      $result = mysqli_query($conn, $sql);
+      while($row = mysqli_fetch_assoc($result)){
+        //  echo $row['category_id'];
+        //  echo $row['category_name'];
+        $cat = $row['category_name'];
+        $desc = $row['category_description'];
+        echo ' <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://source.unsplash.com/random/400X500/?' .$cat. ',coding" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">' .$cat. '</h5>
+            <p class="card-text">' . substr($desc, 0, 90). '... </p>
+            <a href="#" class="btn btn-primary">View Threads</a>
+          </div>
+        </div>
+      </div>';
+      }
+    
+    ?>
+      <!-- Use a for loop to iterate through categories  -->
+     
+
+
+    </div>
+  </div>
+
   <?php include 'partials/_footer.php'; ?>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
 </html>
--- xk-forum/partials/_header.php
+++ xk-forum/partials/_header.php
@@ -1,17 +1,17 @@
 <?php
 echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/xkphpt/xk-forum/">xk forum</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active" aria-current="page" href="/xkphpt/xk-forum/">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="about.php">About</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -25,7 +25,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         </ul>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link" href="contact.php">Contact</a>
         </li>
     </ul>
     <div class="mx-2 d-flex">
@@ -33,13 +33,15 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-success" type="submit">Search</button>
         </form>
-        <button class="ms-2 btn btn-outline-success">Log In</button>
-        <button class="mx-2 btn btn-outline-success">Sign Up</button>
+        <button class="ms-2 btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#loginModal">Log In</button>
+        <button class="mx-2 btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#signupModal">Sign Up</button>
     </div>
  
   </div>
 </div>
 </nav>';
 
+include 'partials/_loginModal.php';
+include 'partials/_signupModal.php';
 
 ?>
